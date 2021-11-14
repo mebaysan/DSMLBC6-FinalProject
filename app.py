c98cb3b9f1eae5ae42e5cb864dab4b11b613342b
Baysan <46582456+mebaysan@users.noreply.github.com>
2021-11-14 20:22:04 +0300

    updated

--- app.py
+++ app.py
@@ -8,18 +8,31 @@ from helpers.model_evaluation import plot_importance
 #################################
 #### * Train ####
 #################################
+
 df = get_data()
 df = asel_mebaysan_preprocess(df, 'group')
 X = df.drop('satisfaction_satisfied', axis=1)
 y = df['satisfaction_satisfied']
 
-result_dict = random_forest_classifier(X, y)
+result_dict_rf = random_forest_classifier(X, y)
 result_dict_xgbm = xgbm_classifier(X, y)
 
 #################################
 #### * Test ####
 #################################
-test_df, test_X, test_y = get_test_data()
-test_dict = predict(test_X, test_y, result_dict_xgbm['model'])
 
-plot_importance(test_dict['model'],test_X,num=15)
+test_df, test_X, test_y = get_test_data(na='group')
+
+test_dict_rf = predict(test_X, test_y, result_dict_rf['model'])
+test_dict_xgbm = predict(test_X, test_y, result_dict_xgbm['model'])
+
+
+#################################
+#### * Plot Importance for Random Forest ####
+#################################
+plot_importance(test_dict_rf['model'],test_X,num=15)
+
+#################################
+#### * Plot Importance for XGBM ####
+#################################
+plot_importance(test_dict_xgbm['model'],test_X,num=15)
